ae21b81bd2cdd04accee1e16ffe431f88495c9d7
WARParrot <starichenko2007@gmail.com>
2026-02-15 16:40:46 +0700

    Added interfaces for blocks

---
+++ src/BlockInterface.tsx
@@ -0,0 +1,14 @@
+﻿export interface IBlockVisual {
+    id: number;
+    name: string;
+    inputField: boolean;
+    inputNode: boolean;
+    outputField: boolean;
+    outputNode: boolean;
+}
+
+export interface IBlockExecutable {
+    id: number;
+    method: Function;
+    next: IBlockExecutable;
+}
