e3e0dcbc7b97b6a8d64d8001ee424d530c32cba0
Dany <104306612+Barkov-Danil@users.noreply.github.com>
2026-02-17 00:17:08 +0700

    create IntegerNumber, add setValue in Variables

--- src/blocks/Variables.tsx
+++ src/blocks/Variables.tsx
@@ -2,32 +2,39 @@ import type { IValue } from './BlockClasses';
 
 export class IntegerVar implements IValue {
     name: string;
-    value: number;
+    private value: number;
 
-    constructor(name: string, value: number) {
+    constructor(name: string, value: number = 0) {
         this.name = name;
         this.value = value;
     }
 
-    getValue(): number | boolean {
+    getValue(): number {
         return this.value;
     }
 
-    AddNew(): void {
+    setValue(value: number): void {
+        this.value = value;
     }
+
+    AddNew(): void {}
 }
 
 export class Boolean implements IValue {
     name: string;
     value: boolean;
 
-    constructor(name: string, value: boolean) {
+    constructor(name: string, value: boolean = false) {
         this.name = name;
         this.value = value;
     }
 
-    getValue(): number | boolean {
+    getValue(): boolean {
         return this.value;
+    }
+
+    setValue(value: boolean): void {
+        this.value = value;
     }
 
     AddNew(): void {
