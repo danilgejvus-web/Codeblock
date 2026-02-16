991da7af7cbace30d3ab5d2c34bfae2e12218d7c
WARParrot <starichenko2007@gmail.com>
2026-02-16 12:16:23 +0700

    Removed redundant interfaces, added base classes

---
+++ src/BlockClasses.tsx
@@ -0,0 +1,31 @@
+﻿export class BlockVisual {
+    id: number;
+    name: string;
+    inputField: boolean;
+    inputNode: boolean;
+    outputField: boolean;
+    outputNode: boolean;
+
+    constructor(id: number, name: string, inputField: boolean, inputNode: boolean, outputField: boolean, outputNode: boolean) {
+        this.id = id;
+        this.name = name;
+        this.inputField = inputField;
+        this.inputNode = inputNode;
+        this.outputField = outputField;
+        this.outputNode = outputNode;
+    }
+}
+
+export class BlockExecutable {
+    id: number;
+    next: BlockExecutable;
+
+    constructor(id: number, next: BlockExecutable) {
+        this.id = id;
+        this.next = next;
+    }
+
+    function Execute(): void {
+        this.next.Execute();
+    }
+}
