be6fed6031e51d9a21d78de7e8e28fa360de92a5
WARParrot <starichenko2007@gmail.com>
2026-03-04 00:17:55 +0700

    Implemented if conditional branching.

---
+++ src/blocks/logic/IfBlock.tsx
@@ -0,0 +1,14 @@
+import type { ExecutableBlock, ExecutionContext, ExecutionInput, ExecutionOutput } from "../ExecutableBlock";
+
+export class IfBlock implements ExecutableBlock {
+    execute(inputs: ExecutionInput, _context: ExecutionContext): ExecutionOutput {
+        const bool = inputs['bool'];
+        const passValue = inputs['input'];
+
+        if (bool == true) {
+            return { true: passValue };
+        } else {
+            return { false: passValue };
+        }
+    }
+}
