348c8b3f8f4838e4f7c324e2f49dc701b156eb8a
WARParrot <starichenko2007@gmail.com>
2026-03-11 17:13:51 +0700

    feat: add FunctionBlock

---
+++ src/blocks/variable/FunctionBlock.tsx
@@ -0,0 +1,16 @@
+import type { ExecutableBlock, ExecutionContext, ExecutionInput, ExecutionOutput } from "../ExecutableBlock";
+
+export class FunctionBlock implements ExecutableBlock {
+    execute(inputs: ExecutionInput, context: ExecutionContext): ExecutionOutput {
+        const subGraph = context.getSubGraph();
+
+        const input = inputs['in'];
+        const subContext = context.newSubContext();
+        const subOuts = context.executeSubGraph(subGraph, input, subContext);
+
+        const subOutputID = subGraph.out.get('out');
+        const subOutput = subOuts.get(subOutputID!);
+
+        return { outputs: subOutput };
+    }
+}
